43361d6373ad5113e2226079db5b12c8005335e9
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2024-09-17 12:50:53 -0500

    repository incompleto

--- src/main/java/uniandes/edu/co/proyecto/repositories/CiudadRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositories/CiudadRepository.java
@@ -1,5 +1,17 @@
 package uniandes.edu.co.proyecto.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public class CiudadRepository {
+import java.util.Collection;
+import uniandes.edu.co.proyecto.modelo.Ciudad;
+
+public interface CiudadRepository extends JpaRepository<Ciudad, Integer>{
+
+    public interface RespuestaInformacionCiudad {
+        
+        
+    }
+    @Query(value = "SELECT * FROM CIUDAD", nativeQuery = true)
+    Collection<Ciudad>darCiudades();
     
 }
