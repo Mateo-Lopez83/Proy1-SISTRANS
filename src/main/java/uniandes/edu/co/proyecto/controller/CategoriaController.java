5a39ae3bdb6d0f16086527fa2ec17ed48c63e3e3
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2024-11-28 21:55:03 -0500

    yeah it works

--- src/main/java/uniandes/edu/co/proyecto/controller/CategoriaController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/CategoriaController.java
@@ -14,7 +14,7 @@ import uniandes.edu.co.proyecto.modelo.Categoria;
 import uniandes.edu.co.proyecto.repositories.CategoriaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
